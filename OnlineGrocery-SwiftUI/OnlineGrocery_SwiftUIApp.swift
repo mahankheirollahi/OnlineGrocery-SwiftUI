4d8e6aa53b23a85fe69a211af5c922717ffae103
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-10-25 12:08:32 +0300

    Initial Commit

---
+++ OnlineGrocery-SwiftUI/OnlineGrocery_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGrocery_SwiftUIApp.swift
+//  OnlineGrocery-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGrocery_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlineGrocery-SwiftUITests/OnlineGrocery_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGrocery_SwiftUITests.swift
+//  OnlineGrocery-SwiftUITests
+//
+//  Created by Mahan Kheirollahi on 10/25/24.
+//
+
+import Testing
+@testable import OnlineGrocery_SwiftUI
+
+struct OnlineGrocery_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
